6e05cbf80bdb8276ab39f8254366413596150380
Pinar Tokmak <ptokmak@hotmail.com>
2016-06-11 00:04:48 -0400

    added movie function

--- liri.js
+++ liri.js
@@ -45,3 +45,29 @@ function myTweets(){
 }//end function myTweets
 
 
+//function to search omdbapi for a movie
+function movieThis() {
+	var value = process.argv[3] || "Mr. Nobody";
+	var queryUrl = 'http://www.omdbapi.com/?&y=&plot=short&r=json&tomatoes=true&t=' + value;
+	request(queryUrl, function(err, response, body) {
+	if ( err ) {
+        console.log('error detail: ' + err);
+        return;
+    } else {
+		body = JSON.parse(body);
+		console.log("Title: " + body.Title);
+		console.log("Year: " + body.Year);
+		console.log("IMDB_Rating: " + body.imdbRating);
+		console.log("Country: " + body.Country);
+		console.log("Language: " + body.Language);
+		console.log("Plot: " + body.Plot);
+		console.log("Actors :" + body.Actors);
+		console.log("Rotten Tomatoes Rating: " + body.tomatoRating);
+		console.log("Rotten Tomatoes URL: " + body.tomatoURL);
+
+	}
+})
+}//end function movieThis
+
+
+
